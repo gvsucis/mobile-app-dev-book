76771c2a4de1896cf4efc4d627d715cb4a6479dd
Jonathan Engelsma <jengelsma@gmail.com>
2017-02-03 11:18:55 -0500

    Added Chapter 4 example code.

---
+++ Android/ch04/Traxy/app/src/main/java/cis/gvsu/edu/traxy/MainActivity.java
@@ -0,0 +1,54 @@
+package cis.gvsu.edu.traxy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent payload = getIntent();
+        if (payload.hasExtra("email")) {
+            String email = payload.getStringExtra("email");
+            TextView user = (TextView)findViewById(R.id.user);
+            user.setText(email);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_logout) {
+            finish();
+            return true;
+        }
+        return false;
+    }
+}
